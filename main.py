959d573325a9c40ab4c80d9a998f0fda7fae324a
Josh Burne <joshb.contact@gmail.com>
2025-05-31 16:28:58 +0100

    added player sprite and circle shape object

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
+    # Boot Up:
     print("Starting Asteroids!")
     pygame.init()
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_object = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT /2)
+    dt = 0
+
+
+    # game loop
     while True:
-        #allow the window to be quit
+        # allow the window to be quit
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        #fill the screen with the RGB colour in the brackets
+        # fill the screen with the RGB colour in the brackets
         screen.fill("#151515")
 
-        #Next frame (ie. next flipbook page)
+        player.draw(screen)
+
+        # Next frame (ie. next flipbook page)
         pygame.display.flip()
+        time_passed = clock_object.tick(60)
+        dt = time_passed / 1000
+        
+
 
 
 
